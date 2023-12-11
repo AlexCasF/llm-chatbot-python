de1bdc0bdd89993ec4d965f1d629cb40cfcd0a53
Alexander Casimir Fischer <131479753+AlexCasF@users.noreply.github.com>
2023-12-11 19:54:13 -0300

    modified:   agent.py modified:   bot.py

--- agent.py
+++ agent.py
@@ -1,5 +1,14 @@
 from langchain.agents import AgentType, initialize_agent
 from llm import llm
+from langchain.chains.conversation.memory import ConversationBufferWindowMemory
+
+memory = ConversationBufferWindowMemory(
+    memory_key='chat_history',
+    k=5,
+    return_messages=True,
+)
+
+tools = []
 
 agent = initialize_agent(
     tools,
@@ -8,3 +17,13 @@ agent = initialize_agent(
     verbose=True,
     agent=AgentType.CHAT_CONVERSATIONAL_REACT_DESCRIPTION,
 )
+
+def generate_response(prompt):
+    """
+    Create a handler that calls the Conversational agent
+    and returns a response to be rendered in the UI
+    """
+
+    response = agent(prompt)
+
+    return response['output']
